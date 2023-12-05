ac8a9acaa6b6790db6247b537e614752b84571c4
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-12-05 17:52:15 +0100

    up

--- src/app/components/expense-chart/expense-chart.component.ts
+++ src/app/components/expense-chart/expense-chart.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Chart, ChartDataset } from 'chart.js/auto';
 import { Client, Report } from 'src/app/model/client';
 
@@ -19,7 +25,8 @@ export class ExpenseChartComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (
       (changes['updateChartTrigger'] || changes['selectedCategory']) &&
-      (!changes['updateChartTrigger']?.firstChange || !changes['selectedCategory']?.firstChange)
+      (!changes['updateChartTrigger']?.firstChange ||
+        !changes['selectedCategory']?.firstChange)
     ) {
       this.generateChart();
     }
@@ -28,42 +35,47 @@ export class ExpenseChartComponent implements OnInit, OnChanges {
   generateChart() {
     if (this.client) {
       const categoriesData: Record<string, Record<string, number>> = {};
-
       this.client.expenses.forEach((expense: Report) => {
         const createdDate: Date = new Date(expense.created);
         const dayMonthYear: string = createdDate.toISOString().split('T')[0];
         categoriesData[expense.type] = categoriesData[expense.type] || {};
-        categoriesData[expense.type][dayMonthYear] = (categoriesData[expense.type][dayMonthYear] || 0) + expense.amount!;
+        categoriesData[expense.type][dayMonthYear] =
+          (categoriesData[expense.type][dayMonthYear] || 0) + expense.amount!;
       });
 
-      const labels: string[] = Object.keys(categoriesData).flatMap(category =>
-        Object.keys(categoriesData[category])
-      ).sort();
+      const labels: string[] = Object.keys(categoriesData)
+        .flatMap((category) => Object.keys(categoriesData[category]))
+        .sort();
 
-      const totalExpenses = labels.map(date =>
-        this.client!.expenses
+      const totalExpenses = labels.map((date) =>
+        this.client!.expenses.filter(
+          (expense) =>
+            expense.type === this.selectedCategory ||
+            this.selectedCategory === 'All'
+        )
           .filter(
-            expense => expense.type === this.selectedCategory || this.selectedCategory === 'All'
-          )
-          .filter(
-            expense =>
+            (expense) =>
               new Date(expense.created).toISOString().split('T')[0] === date
           )
           .reduce((total, expense) => total + expense.amount!, 0)
       );
 
-      const datasets: ChartDataset[] = Object.keys(categoriesData).map(category => {
-        const values = labels.map(
-          label => categoriesData[category][label] || 0
-        );
+      const datasets: ChartDataset[] = Object.keys(categoriesData).map(
+        (category) => {
+          const values = labels.map(
+            (label) => categoriesData[category][label] || 0
+          );
 
-        return {
-          label: `${category}`,
-          data: values,
-          borderWidth: 1,
-          hidden: category !== this.selectedCategory && this.selectedCategory !== 'All',
-        };
-      });
+          return {
+            label: `${category}`,
+            data: values,
+            borderWidth: 1,
+            hidden:
+              category !== this.selectedCategory &&
+              this.selectedCategory !== 'All',
+          };
+        }
+      );
 
       datasets.push({
         label: 'Total Expenses',
@@ -102,6 +114,8 @@ export class ExpenseChartComponent implements OnInit, OnChanges {
   }
 
   getDistinctCategories(): string[] {
-    return this.client ? Array.from(new Set(this.client.expenses.map(expense => expense.type))) : [];
+    return this.client
+      ? Array.from(new Set(this.client.expenses.map((expense) => expense.type)))
+      : [];
   }
 }
